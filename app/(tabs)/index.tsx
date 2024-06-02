1ecc62d65b3bca7e2e86bebe01b94714bebcc017
DeveloperMindset123 <adas006@citymail.cuny.edu>
2024-06-01 20:08:54 -0400

    feat:landing screen done

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -101,13 +101,20 @@ export default function LandingView(this: any, props: any, ButtonProp: any) {
         <Text style={LandingScreenStyle.SubheadingTextStyling}>
           {props.text ?? defaultSubheadingProps.text}
         </Text>
-        <Image
+        <View
           style={{
+            flexDirection: "column",
             alignContent: "center",
-            marginLeft: 20,
+            alignItems: "center",
+            justifyContent: "center",
+            paddingTop: 90,
           }}
-          source={require("@/assets/images/IconArrowRight.png")}
-        />
+        >
+          <Image
+            // import the arrow icon
+            source={require("@/assets/images/IconArrowRight.png")}
+          />
+        </View>
 
         <Button title="" />
       </View>
