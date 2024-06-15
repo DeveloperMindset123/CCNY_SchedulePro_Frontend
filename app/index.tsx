25a56ef1a103bccb011284c5456c765cde868a3a
DeveloperMindset123 <adas006@citymail.cuny.edu>
2024-06-15 09:49:05 -0400

    feat:preliminary rendering of svg asset, also learned how SVGs work in react-native

--- app/index.tsx
+++ app/index.tsx
@@ -7,6 +7,7 @@ import {
 } from "react-native";
 import { Stack, Link } from "expo-router";
 import { Image } from "expo-image";
+import Svg, { Path } from "react-native-svg";
 
 // Declare the relevant Default Routes and Animations Here
 export default function Home() {
@@ -39,15 +40,27 @@ export default function Home() {
       <Text className="flex items-center justify-center text-white text-2xl leading-8 -translate-y-10">
         Class Schedule Manager
       </Text>
-      <Link
-        className="text-white"
-        href={{
-          // Note that the pathname specified can be directly pointing to the specific file, representing the screen within app directory
-          pathname: "./details",
-        }}
-      >
-        Go To Details
-      </Link>
+
+      <View className="items-center justify-center bg-white rounded-full mb-5 py-2 px-2">
+        {/**
+         * TODO : convert to formal JSDOC for improved readabillity
+         * Include SVG asset here, note that this is how svg assets ought to be inclided, simply download and specify the styling and path, replace path with Path and svg with Svg, both of which can be imported from react-native svg */}
+        <Link
+          //className="text-white"
+          href={{
+            // Note that the pathname specified can be directly pointing to the specific file, representing the screen within app directory
+            pathname: "./details",
+          }}
+        >
+          <Svg
+            //xmlns="http://www.w3.org/2000/svg"
+            className="w-10 h-10 overflow-hidden fill-black"
+            viewBox="0 0 448 512"
+          >
+            <Path d="M190.5 66.9l22.2-22.2c9.4-9.4 24.6-9.4 33.9 0L441 239c9.4 9.4 9.4 24.6 0 33.9L246.6 467.3c-9.4 9.4-24.6 9.4-33.9 0l-22.2-22.2c-9.5-9.5-9.3-25 .4-34.3L311.4 296H24c-13.3 0-24-10.7-24-24v-32c0-13.3 10.7-24 24-24h287.4L190.9 101.2c-9.8-9.3-10-24.8-.4-34.3z"></Path>
+          </Svg>
+        </Link>
+      </View>
       <Link
         className="text-white"
         href={{
