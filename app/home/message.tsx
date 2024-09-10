2500911be260735495053a51dee928204063017c
DeveloperMindset123 <adas006@citymail.cuny.edu>
2024-09-10 18:57:25 -0400

    feat:nested routing now works

--- app/home/message.tsx
+++
@@ -1,9 +0,0 @@
-import { SafeAreaView, View, Text } from "react-native";
-
-export default function message() {
-  return (
-    <View>
-      <Text>This is the nested message page</Text>
-    </View>
-  );
-}
