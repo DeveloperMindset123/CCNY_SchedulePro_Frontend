3e455b0b1d31ec73b914e6d10d15fa04d3f70c72
DeveloperMindset123 <adas006@citymail.cuny.edu>
2024-09-11 10:55:25 -0400

    chore:additional logic defined

--- src/lib/utils/getFontFamily.ts
+++ src/lib/utils/getFontFamily.ts
@@ -10,6 +10,24 @@ export const getFontFamily = (
     case 'PLAYPEN':
       if (!weight) {
         return fontFamilies.PLAYPEN.regular;
+      } else if (weight === 'thin') {
+        return fontFamilies.PLAYPEN.thin;
+      } else if (weight === 'extralight') {
+        return fontFamilies.PLAYPEN.extraLight;
+      } else if (weight === 'light') {
+        return fontFamilies.PLAYPEN.light;
+      } else if (weight === 'regular') {
+        return fontFamilies.PLAYPEN.regular;
+      } else if (weight === 'medium') {
+        return fontFamilies.PLAYPEN.medium;
+      } else if (weight === 'semibold') {
+        return fontFamilies.PLAYPEN.semiBold;
+      } else if (weight === 'bold') {
+        return fontFamilies.PLAYPEN.bold;
+      } else if (weight === 'extrabold') {
+        return fontFamilies.PLAYPEN.extraBold;
+      } else {
+        return fontFamilies.PLAYPEN.variableFont;
       }
   }
 };
