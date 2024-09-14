22dfd5faa38d24384ccf223c1a9ba1ed26f57904
DeveloperMindset123 <adas006@citymail.cuny.edu>
2024-09-14 12:09:03 -0400

    feat:fixed icon positioning

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,24 +1,3 @@
-/*
-** Default code, use for reference, implementing the original code from before for the landing screen
-import { Text } from '@/components/core/text';
-import { useColorScheme } from 'nativewind';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { Button } from 'react-native';
-import { Link } from 'expo-router';
-
-// Landing Screen, not sure why it doesn't render unless I name it index
-export default function NewHome() {
-  const { colorScheme } = useColorScheme();
-
-  return (
-    <SafeAreaView>
-      <Text>This should be the new home page</Text>
-      <Link href="/(root)">Go to root</Link>
-    </SafeAreaView>
-  );
-}
-*/
-
 import { View, Text, TouchableOpacity, Image, StyleSheet, Animated, ViewStyle } from 'react-native';
 import { Link } from 'expo-router';
 import React, { useEffect } from 'react';
@@ -67,7 +46,7 @@ const Landing: React.FC = () => {
       >
         <Image
           // m-auto needed to be added to center object
-          className="w-80 h-80 justify-center object-none object-center m-auto mt-6"
+          className="w-80 h-80 justify-center object-none object-center m-auto mt-1"
           source={imagePath}
           width={40}
           height={40}
@@ -76,7 +55,7 @@ const Landing: React.FC = () => {
           style={{
             fontFamily: 'Sofadi',
           }}
-          className="flex text-white font-Pacifico font-semibold text-5xl items-center justify-center mx-auto mt-12"
+          className="flex text-white font-Pacifico font-semibold text-5xl items-center justify-center mx-auto mt-6"
         >
           CCNY
         </Text>
@@ -88,31 +67,22 @@ const Landing: React.FC = () => {
         >
           Class Schedule Manager
         </Text>
-        <TouchableOpacity>
-          <Link
-            href={{
-              pathname: '/authenticationMiddleware',
-            }}
-            className="text-white"
-          >
-            <View
-              style={{
-                justifyContent: 'center',
-                alignContent: 'center',
-              }}
-              className=" bg-white rounded-full mb-5 py-4 px-4"
-            >
-              <Svg width={20} height={20} viewBox="0 0 448 512">
-                <Use x="50%" y="50%">
-                  <Defs>
-                    <Path d="M190.5 66.9l22.2-22.2c9.4-9.4 24.6-9.4 33.9 0L441 239c9.4 9.4 9.4 24.6 0 33.9L246.6 467.3c-9.4 9.4-24.6 9.4-33.9 0l-22.2-22.2c-9.5-9.5-9.3-25 .4-34.3L311.4 296H24c-13.3 0-24-10.7-24-24v-32c0-13.3 10.7-24 24-24h287.4L190.9 101.2c-9.8-9.3-10-24.8-.4-34.3z"></Path>
-                  </Defs>
-                </Use>
-              </Svg>
-            </View>
-          </Link>
-        </TouchableOpacity>
       </FadeInView>
+
+      <TouchableOpacity className="flex-1 justify-center items-center">
+        <Link
+          href={{
+            pathname: '/authenticationMiddleware',
+          }}
+          className="text-white"
+        >
+          <View className="w-15 bg-white rounded-full mb-4 py-4 px-4">
+            <Svg width={35} height={35} viewBox="0 0 448 512">
+              <Path d="M190.5 66.9l22.2-22.2c9.4-9.4 24.6-9.4 33.9 0L441 239c9.4 9.4 9.4 24.6 0 33.9L246.6 467.3c-9.4 9.4-24.6 9.4-33.9 0l-22.2-22.2c-9.5-9.5-9.3-25 .4-34.3L311.4 296H24c-13.3 0-24-10.7-24-24v-32c0-13.3 10.7-24 24-24h287.4L190.9 101.2c-9.8-9.3-10-24.8-.4-34.3z"></Path>
+            </Svg>
+          </View>
+        </Link>
+      </TouchableOpacity>
     </View>
   );
 };
