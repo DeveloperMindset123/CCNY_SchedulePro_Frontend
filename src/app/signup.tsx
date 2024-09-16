61281b067412dbf8292205f96aaea6aea755a2df
DeveloperMindset123 <adas006@citymail.cuny.edu>
2024-09-16 17:02:31 -0400

    feat:working input boxes partially complete

--- src/app/signup.tsx
+++ src/app/signup.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TextInput } from 'react-native';
 import Svg from 'react-native-svg';
 import { SignupIcon } from '@/lib/utils/getSvgs';
 import { TouchableOpacity } from '@/components/core/button';
@@ -8,7 +8,9 @@ import getWindowDimensions from '@/lib/utils/getWindowDimension';
 
 const signup: React.FC = () => {
   const [titleFontStyling, setTitleFontStyling] = useState<string>('');
+  const [secondaryTextStyling, setSecondaryTextStyling] = useState<string>('');
   const [imagePositionStyling, setImagePositionStyling] = useState<string>('');
+  const [emailInput, setEmailInput] = useState<string | any>('');
   const { width, height } = getWindowDimensions();
 
   useEffect(() => {
@@ -16,19 +18,26 @@ const signup: React.FC = () => {
       // ipad styling
       setTitleFontStyling('mt-14 text-white text-[55px]');
       setImagePositionStyling(
-        '-top-52 items-center justify-center mx-auto mb-20 bg-transparent pb-12'
+        '-top-44 items-center justify-center mx-auto mb-20 bg-transparent pb-12'
       );
+      setSecondaryTextStyling('text-white  text-[35px] text-center px-14');
     } else {
       if (height > 700) {
         // larger phones
         setTitleFontStyling('text-white text-3xl mt-8');
-        setImagePositionStyling('-top-52 items-center justify-center mx-auto mb-5');
+        setImagePositionStyling('-top-40 items-center justify-center mx-auto mb-5');
+        setSecondaryTextStyling('text-white mt-2 text-lg text-center px-8');
       } else {
         setTitleFontStyling('text-white text-2xl mt-6');
-        setImagePositionStyling('-top-32 items-center justify-center mx-auto pb-5');
+        setImagePositionStyling('-top-24 items-center justify-center mx-auto pb-5');
+        setSecondaryTextStyling('text-white mt-1 text-md text-center px-10');
       }
     }
   }, [width, height]);
+
+  // TODO : Adjust based on different screen sizes
+  let inputBoxWidth = width * 0.9;
+  let inputBoxHeight = height * 0.07;
 
   return (
     <View className="flex-1 bg-black text-white justify-center items-center">
@@ -44,13 +53,28 @@ const signup: React.FC = () => {
           style={{
             fontFamily: 'playpenLight',
           }}
-          // 2xl for screens of width < 600 but height < 700
-          // 3xl for screens of width < 600 but height > 700
-          // 5xl for screens of width > 600 and height > 700
           className={titleFontStyling}
         >
           Join CCNY Schedule Pro!
         </Text>
+        <Text
+          style={{
+            fontFamily: 'playpenExtraLight',
+          }}
+          className={secondaryTextStyling}
+        >
+          Explore and manage class schedules efficiently
+        </Text>
+        <TextInput
+          style={{
+            width: inputBoxWidth,
+            height: inputBoxHeight,
+          }}
+          className="bg-gray-500 text-white h-[40px] m-[12px] border-spacing-1 p-[10px] rounded-full"
+          onChange={setEmailInput}
+          value={emailInput}
+          keyboardType="default"
+        ></TextInput>
       </View>
     </View>
   );
