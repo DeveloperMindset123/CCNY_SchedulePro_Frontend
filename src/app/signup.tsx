0070ff168321a02569b27e3abd157d79b5e972e6
DeveloperMindset123 <adas006@citymail.cuny.edu>
2024-09-16 17:13:54 -0400

    chore:additional working progress

--- src/app/signup.tsx
+++ src/app/signup.tsx
@@ -11,21 +11,22 @@ const signup: React.FC = () => {
   const [secondaryTextStyling, setSecondaryTextStyling] = useState<string>('');
   const [imagePositionStyling, setImagePositionStyling] = useState<string>('');
   const [emailInput, setEmailInput] = useState<string | any>('');
+  const [passwordInput, setPasswordInput] = useState<string | any>('');
+  const [confirmPasswordInput, setConfirmPasswordInput] = useState<string | any>('');
   const { width, height } = getWindowDimensions();
 
+  // TODO : Continue this implementation
   useEffect(() => {
     if (width > 600 && height > 700) {
       // ipad styling
       setTitleFontStyling('mt-14 text-white text-[55px]');
-      setImagePositionStyling(
-        '-top-44 items-center justify-center mx-auto mb-20 bg-transparent pb-12'
-      );
+      setImagePositionStyling('-top-[48px] items-center justify-center mx-auto  bg-transparent');
       setSecondaryTextStyling('text-white  text-[35px] text-center px-14');
     } else {
       if (height > 700) {
         // larger phones
         setTitleFontStyling('text-white text-3xl mt-8');
-        setImagePositionStyling('-top-40 items-center justify-center mx-auto mb-5');
+        setImagePositionStyling('-top-[62px] items-center justify-center mx-auto mb-5');
         setSecondaryTextStyling('text-white mt-2 text-lg text-center px-8');
       } else {
         setTitleFontStyling('text-white text-2xl mt-6');
@@ -35,8 +36,10 @@ const signup: React.FC = () => {
     }
   }, [width, height]);
 
-  // TODO : Adjust based on different screen sizes
+  // width can be static
   let inputBoxWidth = width * 0.9;
+
+  // height needs to be adjusted based on different screen sizes
   let inputBoxHeight = height * 0.07;
 
   return (
@@ -72,7 +75,30 @@ const signup: React.FC = () => {
           }}
           className="bg-gray-500 text-white h-[40px] m-[12px] border-spacing-1 p-[10px] rounded-full"
           onChange={setEmailInput}
+          placeholder="Enter Your Email"
           value={emailInput}
+          keyboardType="default"
+        ></TextInput>
+        <TextInput
+          style={{
+            width: inputBoxWidth,
+            height: inputBoxHeight,
+          }}
+          className="bg-gray-500 text-white h-[40px] m-[12px] border-spacing-1 p-[10px] rounded-full"
+          onChange={setPasswordInput}
+          placeholder="Enter Your Password"
+          value={passwordInput}
+          keyboardType="default"
+        ></TextInput>
+        <TextInput
+          style={{
+            width: inputBoxWidth,
+            height: inputBoxHeight,
+          }}
+          className="bg-gray-500 text-white h-[40px] m-[12px] border-spacing-1 p-[10px] rounded-full"
+          onChange={setConfirmPasswordInput}
+          placeholder="Confirm Your Password"
+          value={confirmPasswordInput}
           keyboardType="default"
         ></TextInput>
       </View>
