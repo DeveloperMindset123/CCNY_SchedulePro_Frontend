c2cad292614ea9a87898978950c88e4fcd4d4eec
DeveloperMindset123 <adas006@citymail.cuny.edu>
2024-10-18 12:12:01 -0400

    chore:minor progress with the slider component

--- src/app/onboarding3.tsx
+++ src/app/onboarding3.tsx
@@ -3,6 +3,9 @@ import { View, TextInput, Keyboard, Text } from 'react-native';
 import { useRouter } from 'expo-router';
 import { OnboardingButton } from '@/components/core/button/onboarding-buttons';
 import getWindowDimensions from '@/lib/utils/getWindowDimension';
+import { useSharedValue } from 'react-native-reanimated';
+import { Slider } from 'react-native-awesome-slider';
+import * as ReactNatieHapticsFeedback from 'expo-haptics';
 
 /**
  *
@@ -19,6 +22,7 @@ const OnboardingScreen3: React.FC = () => {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [lastName, setLastName] = useState<string>('');
   // TODO : This might be somewhat redundant, for learning new hooks, because this is just excessive use of hooks at the end of the day that leads to performance degradation
+  // TODO : wrap the textInput components around a map component as before as well
   const firstNamePlaceholder = useRef('First Name...');
   const lastNamePlaceholder = useRef('Last Name...');
   const { width, height } = getWindowDimensions();
@@ -27,6 +31,10 @@ const OnboardingScreen3: React.FC = () => {
     height: height * 0.07,
   };
   const router = useRouter();
+  // we also need to take into account phD and masters student
+  const progress = useSharedValue(8);
+  const min = useSharedValue(0);
+  const max = useSharedValue(16);
 
   return (
     <View className="bg-black flex-1">
@@ -58,6 +66,30 @@ const OnboardingScreen3: React.FC = () => {
           onSubmitEditing={Keyboard.dismiss}
           placeholderTextColor="#94a3b8"
           autoCorrect={false}
+        />
+      </View>
+      <View className="flex-col">
+        <Slider
+          // Does not support tailwind
+          style={{
+            width: width * 0.9,
+            alignContent: 'center',
+            marginHorizontal: 'auto',
+          }}
+          theme={{
+            disableMinTrackTintColor: 'purple',
+            maximumTrackTintColor: 'white',
+            minimumTrackTintColor: 'purple',
+            cacheTrackTintColor: 'white',
+            bubbleBackgroundColor: 'black',
+            heartbeatColor: '#999',
+          }}
+          progress={progress}
+          minimumValue={min}
+          maximumValue={max}
+          bubbleMaxWidth={width * 0.9}
+          step={Math.round(16)}
+          heartbeat={true}
         />
       </View>
       <View className="flex-row">
