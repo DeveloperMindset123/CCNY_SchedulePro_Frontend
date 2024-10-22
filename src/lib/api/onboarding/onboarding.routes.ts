e1c7abf5a8dcde171ca4c98a57924821ced406fa
DeveloperMindset123 <adas006@citymail.cuny.edu>
2024-10-21 23:27:32 -0400

    chore:additional API related logic

--- src/lib/api/onboarding/onboarding.routes.ts
+++ src/lib/api/onboarding/onboarding.routes.ts
@@ -2,7 +2,6 @@ import { Router } from 'express';
 
 const onboardingRouter = Router();
 // TODO : Define a function that will handle autoamtically calling the data using axios
-// TODO : the axios method will be a get method, since we want to return the data
 onboardingRouter.post('/onboarding2Data', async (req, res, next) => {
   try {
     const { degree, major } = req.body;
@@ -10,6 +9,10 @@ onboardingRouter.post('/onboarding2Data', async (req, res, next) => {
       res.status(400);
       throw new Error('Either degree or major field is empty');
     } else {
+      setOnboarding2Data({
+        degree: degree,
+        major: major,
+      });
       res.send({
         degree: degree,
         major: major,
@@ -21,3 +24,23 @@ onboardingRouter.post('/onboarding2Data', async (req, res, next) => {
     next(err);
   }
 });
+
+onboardingRouter.post('/onboarding3Data', async (req, res, next) => {
+  // Not sure if this is fully correct or not
+  const data = req.body;
+  if (!data) {
+    res.status(400);
+    throw new Error('Some of the data is missing');
+  } else {
+    setOnboarding3Data(data);
+    res.send(data);
+  }
+});
+
+export const setOnboarding2Data = (onboarding2Data: any) => {
+  return onboarding2Data;
+};
+
+export const setOnboarding3Data = (onboarding3Data: any) => {
+  return onboarding3Data;
+};
--- src/lib/api/onboarding/onboarding.services.ts
+++ src/lib/api/onboarding/onboarding.services.ts
@@ -1,6 +1,8 @@
 // TODO : define database logic here
 import { db } from '@/lib/utils/db';
 import { Prisma } from '@prisma/client';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { setOnboarding2Data } from './onboarding.routes';
 
 export const insertUserInformationDetails = (RegisteredUser: any) => {
   const UserDetail: Prisma.UserRegistrationDetailsCreateInput = {
